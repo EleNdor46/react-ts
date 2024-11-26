9d351ba07385e17a86bea5ddfcf27fbf9d9906c5
EleNdor <amo123beach@gmail.com>
2024-11-26 11:43:28 +0300

    upgrate webpack config

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -5,12 +5,14 @@ interface BuildBabelLoaderProps extends BuildOptions {
 }
 
 export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps) {
+    const isProd = !isDev;
     return {
         test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
         exclude: /node_modules/,
         use: {
             loader: "babel-loader",
             options: {
+                cacheDirectory: true,
                 presets: ["@babel/preset-env"],
                 plugins: [
                     [
